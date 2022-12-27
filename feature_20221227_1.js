bf7c36b6dc64fe7d05c3731001fb1067fcf4556b
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2022-12-27 10:47:00 +0530

    chore: bump version to 1.2.0

---
+++ feature_20221227_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2022-12-27
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2022-12-27
+    return null;
+  };
+// chore: bump version to 1.2.0 (2022-12-27)
