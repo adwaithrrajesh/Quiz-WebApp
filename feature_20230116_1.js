4d03607c6c61324a073e6504775bbcb7afe6e08d
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-01-16 17:58:00 +0530

    feat: add logging middleware

---
+++ feature_20230116_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-01-16
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-01-16
+    return null;
+  };
+// feat: add logging middleware (2023-01-16)
