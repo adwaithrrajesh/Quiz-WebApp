0a37614e9de025244d7e048cc8f859e49aec6c08
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-01-24 18:07:00 +0530

    docs: update usage section

---
+++ feature_20230124_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-01-24
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-01-24
+    return null;
+  };
+// docs: update usage section (2023-01-24)
