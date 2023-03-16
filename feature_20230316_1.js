0a4d9ed91279552509cc3281713640bc860b0e4d
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-03-16 17:36:00 +0530

    fix: typo in README

---
+++ feature_20230316_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-03-16
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-03-16
+    return null;
+  };
+// fix: typo in README (2023-03-16)
