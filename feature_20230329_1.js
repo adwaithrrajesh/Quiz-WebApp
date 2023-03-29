bfef79c180fe77f43518ca4ae0984f3975fae162
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-03-29 08:36:00 +0530

    docs: clarify API response format

---
+++ feature_20230329_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-03-29
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-03-29
+    return null;
+  };
+// docs: clarify API response format (2023-03-29)
