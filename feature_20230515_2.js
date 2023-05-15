b602b16373a16060b60dca73012c6266be037f52
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-05-15 19:42:00 +0530

    docs: add API usage example

---
+++ feature_20230515_2.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-05-15
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-05-15
+    return null;
+  };
+// docs: add API usage example (2023-05-15)
