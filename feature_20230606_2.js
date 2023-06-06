2c7f007a38b9d29bd902e9ab129dfa11e68f5fae
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-06-06 18:32:00 +0530

    test: add basic tests

---
+++ feature_20230606_2.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-06-06
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-06-06
+    return null;
+  };
+// test: add basic tests (2023-06-06)
