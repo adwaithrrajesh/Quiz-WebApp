169c9180bc29656a96605b6a9942aad062c22903
“adwaithrrajesh” <adwaithrrajesh@hotmail.com>
2023-07-20 20:03:00 +0530

    fix: handle null pointer in service

---
+++ feature_20230720_1.js
@@ -0,0 +1,6 @@
+  // Dummy feature file created on 2023-07-20
+  module.exports = function dummyFeature() {
+    // TODO: implement on 2023-07-20
+    return null;
+  };
+// fix: handle null pointer in service (2023-07-20)
